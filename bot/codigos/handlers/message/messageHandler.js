1dd765faf0879a5cff210677d873214eece69ce7
Lucas Nascimento <laravelcrmsysttem@gmail.com>
2026-09-05 02:28:01 -0300

    nova alteracao

--- bot/codigos/handlers/message/messageHandler.js
+++ bot/codigos/handlers/message/messageHandler.js
@@ -159,7 +159,7 @@ const MODELO_POSTER =
 // ============================================
 // 🏠 IDs DOS GRUPOS
 // ============================================
-const GRUPO_PRINCIPAL = '120363413774574277@g.us';
+const GRUPO_PRINCIPAL = '120363412511975026@g.us';
 const GRUPO_ADMINS = '120363409228091157@g.us';
 const GRUPO_CONTROLE = '120363409394983918@g.us';
 
